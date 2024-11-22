406f0a59826c84a8e17a15d9fc75091e01bbf5d3
Max Fu <maxfu@Maxs-MacBook-Pro-2.local>
2024-11-21 17:05:37 -0800

    more commits

--- api/api.go
+++ api/api.go
@@ -1,3 +1,10 @@
+// A set of rules and standard for requests and responses between the client and server 
 package api
 
+import (
+	"ecoding/json"
+	"net/http"
+)
+
 //Write Parameters struct which represents the parameters our API endpoint will take
+type 
